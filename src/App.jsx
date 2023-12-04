079246cc2f8776aae0d54eda91d051ba1e6da9f9
bszarlowicz <b.szarlowicz@gmail.com>
2023-12-04 21:44:13 +0100

    file structure - config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import StartPage from './pages/StartPage'
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<StartPage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
